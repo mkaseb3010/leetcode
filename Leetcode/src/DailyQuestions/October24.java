ca3f2b51405e6ffc607bde932ddf0ef8d2907c21
mkaseb3010 <mazen.kaseb@gmail.com>
2024-10-30 17:06:27 +0300

    Added October 24 Implementation + Test cases

---
+++ Leetcode/src/DailyQuestions/October24.java
@@ -0,0 +1,38 @@
+package DailyQuestions;
+
+public class October24 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution {
+        public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+            if (root1 == null && root2 == null) {
+                return true;
+            }
+
+            if (root1 == null || root2 == null || root1.val != root2.val) {
+                return false;
+            }
+
+            boolean flip = flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left);
+            boolean noFlip = flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right);
+
+            if (flip || noFlip) {
+                return true;
+            }else {
+                return false;
+            }
+        }
+    }
+}
