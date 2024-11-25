988f6c1c3a440cf259f2c473711c57f13c732d31
mkaseb3010 <mazen.kaseb@gmail.com>
2024-11-25 14:21:15 +0300

    Added Top 6 - Top 24 Solutions

---
+++ Leetcode/src/Top150/Top21.java
@@ -0,0 +1,20 @@
+package Top150;
+
+public class Top21 {
+    public class Solution {
+        public String reverseWords(String s) {
+            s = s.trim();
+            String[] words = s.split("\\s+");
+            StringBuilder result = new StringBuilder();
+    
+            for (int i = words.length - 1; i >= 0; i--) {
+                result.append(words[i]);
+                if (i != 0) {
+                    result.append(" ");
+                }
+            }
+            String str = result.toString();
+            return str;
+        }
+    }
+}
