02c4ad1ebf370a47f9ad60e3803c0ce50fd0e964
mkaseb3010 <mazen.kaseb@gmail.com>
2025-02-19 19:05:16 +0300

    Implemented leetcode 1415.

---
+++ Leetcode/src/DailyQuestionsFebruary/February19.java
@@ -0,0 +1,31 @@
+package DailyQuestionsFebruary;
+
+public class February19 {
+    class Solution {
+        private void backtrack(List<String> result, StringBuilder current, int n) {
+            if (current.length() == n) {
+                result.add(current.toString());
+                return;
+            }
+    
+            for (char ch : new char[]{'a', 'b', 'c'}) {
+                if (current.length() == 0 || current.charAt(current.length() - 1) != ch) {
+                    current.append(ch);
+                    backtrack(result, current, n);
+                    current.deleteCharAt(current.length() - 1);
+                }
+            }
+        }
+    
+        public String getHappyString(int n, int k) {
+            List<String> result = new ArrayList<>();
+            backtrack(result, new StringBuilder(), n);
+    
+            if (k > result.size()) {
+                return "";
+            }
+    
+            return result.get(k - 1);
+        }
+    }
+}
