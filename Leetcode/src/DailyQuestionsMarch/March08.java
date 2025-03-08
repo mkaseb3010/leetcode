22b33745f1aa4f248ccd6e7810dd5533c270de95
mkaseb3010 <mazen.kaseb@gmail.com>
2025-03-08 22:55:46 +0300

    Implemented leetcode 2379.

---
+++ Leetcode/src/DailyQuestionsMarch/March08.java
@@ -0,0 +1,29 @@
+public class March08 {
+    class Solution {
+        public int minimumRecolors(String blocks, int k) {
+            int min = Integer.MAX_VALUE;
+            int count = 0;
+    
+            for (int i = 0; i < k; i++) {
+                if (blocks.charAt(i) == 'W') {
+                    count++;
+                }
+            }
+    
+            min = count;
+    
+            for (int i = k; i < blocks.length(); i++) {
+                if (blocks.charAt(i - k) == 'W') {
+                    count--;
+                }
+    
+                if (blocks.charAt(i) == 'W') {
+                    count++;
+                }
+    
+                min = Math.min(min, count);
+            }
+            return min;
+        }
+    }
+}
