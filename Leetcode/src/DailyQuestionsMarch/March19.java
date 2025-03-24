7bf7372ed20c069f67a73c61c7ffaa7483bfb154
mkaseb3010 <mazen.kaseb@gmail.com>
2025-03-25 00:28:00 +0300

    Implemented leetcode 3191.

---
+++ Leetcode/src/DailyQuestionsMarch/March19.java
@@ -0,0 +1,22 @@
+public class March19 {
+    class Solution {
+        public int minOperations(int[] nums) {
+            int n = nums.length;
+            int count = 0;
+    
+            for (int i = 0; i <= n - 3; i++) {
+                if (nums[i] == 0) {
+                    nums[i] ^= 1;
+                    nums[i + 1] ^= 1;
+                    nums[i + 2] ^= 1;
+                    count++;
+                }
+            }
+    
+            if (nums[n - 1] == 0 || nums[n - 2] == 0) {
+                return -1;
+            }
+            return count;
+        }
+    }
+}
