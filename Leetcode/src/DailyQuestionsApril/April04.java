8451ab60e7843a3bed8204c15ee7f369afd45a23
mkaseb3010 <mazen.kaseb@gmail.com>
2025-05-30 21:23:12 -0400

    Implemented leetcode 1123.

---
+++ Leetcode/src/DailyQuestionsApril/April04.java
@@ -0,0 +1,35 @@
+public class April04 {
+    class Solution {
+        private class Result {
+            TreeNode node;
+            int depth;
+
+            Result(TreeNode node, int depth) {
+                this.node = node;
+                this.depth = depth;
+            }
+        }
+        private Result dfs(TreeNode node) {
+            if (node == null) {
+                return new Result(null, 0);
+            }
+
+            Result left = dfs(node.left);
+            Result right = dfs(node.right);
+
+            if (left.depth > right.depth) {
+                return new Result(left.node, left.depth + 1);
+            }
+
+            if (right.depth > left.depth) {
+                return new Result(right.node, right.depth + 1);
+            }
+            
+            return new Result(node, left.depth + 1);
+        }
+
+        public TreeNode lcaDeepestLeaves(TreeNode root) {
+            return dfs(root).node;
+        }
+    }
+}
