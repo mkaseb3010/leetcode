bd5e8d6b7ec06f147d01f26962f9057f0c52a418
mkaseb3010 <mazen.kaseb@gmail.com>
2025-06-22 22:11:40 +0300

    Implemented leetcode 1922.

---
+++ Leetcode/src/DailyQuestionsApril/April13.java
@@ -0,0 +1,33 @@
+public class April13 {
+    class Solution {
+        public int countGoodNumbers(long n) {
+            if(n==1) {
+                return 5;
+            }
+
+            long even = (n+1)/2;
+            long odd = n/2;
+            long mod = 1000000007;
+        
+            return (int)((goodNumbers(5,even,mod)*goodNumbers(4,odd,mod))%mod);
+        }
+
+        public long goodNumbers(long base,long expo,long mod){
+            long ans = 1;
+
+            while(expo>0)
+            {
+                if(expo%2==0)            
+                {
+                    base = (base*base)%mod;
+                    expo = expo/2;
+                }
+                else{
+                    ans = (ans*base)%mod;
+                    expo-=1;
+                }
+            }
+            return ans;
+        }
+    }
+}
