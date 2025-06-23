1356b9248e1be56152ba67e47922e61767e318dc
mkaseb3010 <mazen.kaseb@gmail.com>
2025-06-23 14:58:19 +0300

    code clean up

--- Leetcode/src/DailyQuestionsApril/April13.java
+++ Leetcode/src/DailyQuestionsApril/April13.java
@@ -5,12 +5,12 @@ import java.util.*;
 public class April13 {
     class Solution {
         public int countGoodNumbers(long n) {
-            if(n==1) {
+            if(n == 1) {
                 return 5;
             }
 
-            long even = (n+1)/2;
-            long odd = n/2;
+            long even = (n + 1)/2;
+            long odd = n / 2;
             long mod = 1000000007;
         
             return (int)((goodNumbers(5,even,mod)*goodNumbers(4,odd,mod))%mod);
@@ -19,16 +19,16 @@ public class April13 {
         public long goodNumbers(long base,long expo,long mod){
             long ans = 1;
 
-            while(expo>0)
+            while(expo > 0)
             {
-                if(expo%2==0)            
+                if(expo % 2 == 0)            
                 {
-                    base = (base*base)%mod;
-                    expo = expo/2;
+                    base = (base*base) % mod;
+                    expo = expo / 2;
                 }
                 else{
-                    ans = (ans*base)%mod;
-                    expo-=1;
+                    ans = (ans*base) % mod;
+                    expo -= 1;
                 }
             }
             return ans;
