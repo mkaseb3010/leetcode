0f40be4461c60e0e8620533ab9fc2509694602cb
mkaseb3010 <mazen.kaseb@gmail.com>
2025-06-23 17:51:39 +0300

    Implemented leetcode 2537.

---
+++ Leetcode/src/DailyQuestionsApril/April16.java
@@ -0,0 +1,28 @@
+public class April16 {
+    class Solution {
+        public long countGood(int[] nums, int k) {
+            int n = nums.length;
+            HashMap<Integer, Integer> freq = new HashMap<>();
+            long res = 0;
+            int left = 0;
+            long pairs = 0;
+
+            for (int right = 0; right < n; right++) {
+                int val = nums[right];
+                int count = freq.getOrDefault(val, 0);
+                pairs += count;
+                freq.put(val, count + 1);
+
+                while (pairs >= k) {
+                    res += (n - right);
+                    int leftVal = nums[left];
+                    int leftCount = freq.get(leftVal);
+                    pairs -= (leftCount - 1);
+                    freq.put(leftVal, leftCount - 1);
+                    left++;
+                }
+            }
+            return res;
+        }
+    }
+}
