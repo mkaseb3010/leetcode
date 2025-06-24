8f8248710398cf4b5321f9b548bdf6e4ddab13f7
mkaseb3010 <mazen.kaseb@gmail.com>
2025-06-24 18:30:51 +0300

    Code cleanup

--- Leetcode/src/DailyQuestionsApril/April18.java
+++ Leetcode/src/DailyQuestionsApril/April18.java
@@ -1,3 +1,5 @@
+package DailyQuestionsApril;
+
 public class April18 {
     class Solution {
         public String countAndSay(int n) {
@@ -5,22 +7,22 @@ public class April18 {
                 return "1";
             }
 
-                String prev = countAndSay(n - 1);
-                StringBuilder sb = new StringBuilder();
-                int count = 1;
+            String prev = countAndSay(n - 1);
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
 
-                for (int i = 1; i < prev.length(); i++) {
-                    if (prev.charAt(i) == prev.charAt(i - 1)) {
-                        count++;
-                    }else {
-                        sb.append(count).append(prev.charAt(i - 1));
-                        count = 1;
-                    }
+            for (int i = 1; i < prev.length(); i++) {
+                if (prev.charAt(i) == prev.charAt(i - 1)) {
+                    count++;
+                }else {
+                    sb.append(count).append(prev.charAt(i - 1));
+                    count = 1;
                 }
-
-                sb.append(count).append(prev.charAt(prev.length() - 1));
-
-                return sb.toString();
             }
+
+            sb.append(count).append(prev.charAt(prev.length() - 1));
+
+            return sb.toString();
+        }
     }
 }
