c07b0071c58cf979a9f60e922ca99bf3ea7e96a5
mkaseb3010 <mazen.kaseb@gmail.com>
2025-07-25 03:00:19 +0300

    Implemented leetcode 2094.

---
+++ Leetcode/src/DailyQuestionsMay/May11.java
@@ -0,0 +1,44 @@
+public class May11 {
+    class Solution {
+        public int[] findEvenNumbers(int[] digits) {
+            Set<Integer> result = new TreeSet<>();
+
+            for (int i = 0; i < digits.length; i++) {
+                for (int j = 0; j < digits.length; j++) {
+                    if (j == i) {
+                        continue;
+                    }
+
+                    for (int k = 0; k < digits.length; k++) {
+                        if (k == i || k == j) {
+                            continue;
+                        }
+
+                        int a = digits[i];
+                        int b = digits[j];
+                        int c = digits[k];
+
+                        if (a == 0) {
+                            continue;
+                        }
+
+                        if (c % 2 != 0) {
+                            continue;
+                        }
+
+                        int num = a * 100 + b * 10 + c;
+                        result.add(num);
+                    }
+                }
+            }
+
+            int[] output = new int[result.size()];
+            int index = 0;
+
+            for (int num : result) {
+                output[index++] = num;
+            }
+            return output;
+        }
+    }
+}
