d15ccd54d0a6ca2f45a4e851f0788ebe6f89dab7
mkaseb3010 <mazen.kaseb@gmail.com>
2025-07-28 23:09:41 +0300

    Implemented leetcode 3024.

---
+++ Leetcode/src/DailyQuestionsMay/May18.java
@@ -0,0 +1,29 @@
+package DailyQuestionsMay;
+
+import java.util.Arrays;
+
+public class May18 {
+    class Solution {
+        public String triangleType(int[] nums) {
+            Arrays.sort(nums);
+
+            int a = nums[0];
+            int b = nums[1];
+            int c = nums[2];
+
+            if (a + b <= c) {
+                return "none";
+            }
+
+            if (a == b && b == c) {
+                return "equilateral";
+            }
+
+            if (a == b || b == c || a == c) {
+                return "isosceles";
+            
+            }
+            return "scalene";
+        }
+    }
+}
