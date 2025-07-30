b2423b96766988db310115594474454786afb389
mkaseb3010 <mazen.kaseb@gmail.com>
2025-07-30 21:15:07 +0300

    Implemented leetcode 3355.

---
+++ Leetcode/src/DailyQuestionsMay/May20.java
@@ -0,0 +1,32 @@
+package src.DailyQuestionsMay;
+
+
+public class May20 {
+    class Solution {
+        public boolean isZeroArray(int[] nums, int[][] queries) {
+            int n = nums.length;
+            int[] freq = new int[n + 1];
+
+            for (int[] q : queries) {
+                int left = q[0];
+                int right = q[1];
+                freq[left]++;
+
+                if (right + 1 < n) {
+                    freq[right + 1]--;
+                }
+            }
+
+            for (int i = 1; i < n; i++) {
+                freq[i] += freq[i - 1];
+            }
+
+            for (int i = 0; i < n; i++) {
+                if (freq[i] < nums[i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
