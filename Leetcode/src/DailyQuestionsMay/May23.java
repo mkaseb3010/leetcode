25ae63c3843f7b766a6d53d170eb2195f8a5b918
mkaseb3010 <mazen.kaseb@gmail.com>
2025-08-04 03:57:01 +0300

    Implemented leetcode 3068.

---
+++ Leetcode/src/DailyQuestionsMay/May23.java
@@ -0,0 +1,15 @@
+public class May23 {
+    class Solution {
+        public long maximumValueSum(int[] nums, int k, int[][] edges) {
+            long[] dp = {0, Long.MIN_VALUE};
+
+            for (int n : nums) {
+                long count1 = dp[0] + n;
+                long count2 = dp[1] + n;
+                int x = n ^ k;
+                dp = new long[]{ Math.max(count1, dp[1] + x), Math.max(count2, dp[0] + x) };
+            }
+            return dp[0];
+        }
+    }
+}
